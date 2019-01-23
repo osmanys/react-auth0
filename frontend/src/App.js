ed8ab160e6a6991a113bfec5530d1c0474d44adc
Osmanys Alonso <osmanys@gmail.com>
2019-01-23 01:21:39 -0300

    Securing react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {Route} from 'react-router-dom';
 import NavBar from './NavBar/NavBar';
 import Question from './Question/Question';
 import Questions from './Questions/Questions';
+import Callback from './Callback';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <NavBar/>
         <Route exact path='/' component={Questions}/>
         <Route exact path='/question/:questionId' component={Question}/>
+        <Route exact path='/callback' component={Callback}/>
       </div>
     );
   }
